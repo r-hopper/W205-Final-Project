12bc9a24e7d3bdb4b323aa490000e5ae6ac62c19
Roseanna Hopper <rhopper@berkeley.edu>
2017-03-30 14:43:01 -0400

    Update load_flight_data_2.sh

--- load_flight_data/load_flight_data_2.sh
+++ load_flight_data/load_flight_data_2.sh
@@ -10,6 +10,10 @@ mkdir ~/staging/final_project
 # Change to staging directory
 cd ~/staging/final_project
 
+# Create main hdfs directory
+hdfs dfs -mkdir /user/w205/final_project
+hdfs dfs -mkdir /user/w205/final_project/flight_data
+
 ##############################
 # Name files transtats.bts.gov
 URL="https://transtats.bts.gov//PREZIP/On_Time_On_Time_Performance_2016_1.zip"
@@ -26,8 +30,6 @@ NEW_FILE="on_time_2016_1.csv"
 tail -n +2 $OLD_FILE > $NEW_FILE
 
 # Create hdfs directories
-hdfs dfs -mkdir /user/w205/final_project
-hdfs dfs -mkdir /user/w205/final_project/flight_data
 hdfs dfs -mkdir /user/w205/final_project/flight_data/2016_1
 
 # Copy the files to hdfs
@@ -56,8 +58,6 @@ NEW_FILE="on_time_2016_2.csv"
 tail -n +2 $OLD_FILE > $NEW_FILE
 
 # Create hdfs directories
-hdfs dfs -mkdir /user/w205/final_project
-hdfs dfs -mkdir /user/w205/final_project/flight_data
 hdfs dfs -mkdir /user/w205/final_project/flight_data/2016_2
 
 # Copy the files to hdfs
@@ -86,8 +86,6 @@ NEW_FILE="on_time_2016_3.csv"
 tail -n +2 $OLD_FILE > $NEW_FILE
 
 # Create hdfs directories
-hdfs dfs -mkdir /user/w205/final_project
-hdfs dfs -mkdir /user/w205/final_project/flight_data
 hdfs dfs -mkdir /user/w205/final_project/flight_data/2016_3
 
 # Copy the files to hdfs
