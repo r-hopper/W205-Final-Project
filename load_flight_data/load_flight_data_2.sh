0d85cbf4714adeac93e0e2d1c7d9e5448a08b666
Roseanna Hopper <rhopper@berkeley.edu>
2017-03-30 14:59:29 -0400

    Update load_flight_data_2.sh

--- load_flight_data/load_flight_data_2.sh
+++ load_flight_data/load_flight_data_2.sh
@@ -97,6 +97,33 @@ rm /home/w205/staging/final_project/On_Time_On_Time_Performance_2016_3.csv
 rm /home/w205/staging/final_project/On_Time_On_Time_Performance_2016_3.zip
 rm /home/w205/staging/final_project/readme.html
 
+##############################
+# Name files transtats.bts.gov
+URL="https://transtats.bts.gov//PREZIP/On_Time_On_Time_Performance_2016_3.zip"
+
+# Get prezipped file
+wget --no-check-certificate "$URL" -O On_Time_On_Time_Performance_2016_3.zip
+
+# Unzip data
+unzip On_Time_On_Time_Performance_2016_3.zip
+
+# Remove the first line of files and rename
+OLD_FILE="On_Time_On_Time_Performance_2016_3.csv"
+NEW_FILE="on_time_2016_3.csv"
+tail -n +2 $OLD_FILE > $NEW_FILE
+
+# Create hdfs directories
+hdfs dfs -mkdir /user/w205/final_project/flight_data/2016_3
+
+# Copy the files to hdfs
+hdfs dfs -put $NEW_FILE /user/w205/final_project/flight_data/2016_3
+
+# Remove from staging directory to free up memory
+rm /home/w205/staging/final_project/on_time_2016_3.csv
+rm /home/w205/staging/final_project/On_Time_On_Time_Performance_2016_3.csv
+rm /home/w205/staging/final_project/On_Time_On_Time_Performance_2016_3.zip
+rm /home/w205/staging/final_project/readme.html
+
 # Change directory back to original
 cd $MY_PWD
 
