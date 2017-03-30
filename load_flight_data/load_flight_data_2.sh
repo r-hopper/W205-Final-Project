1d2b4453512ab6f8777aec757005b0a138f6ebb1
Roseanna Hopper <rhopper@berkeley.edu>
2017-03-30 15:19:23 -0400

    Update load_flight_data_2.sh

--- load_flight_data/load_flight_data_2.sh
+++ load_flight_data/load_flight_data_2.sh
@@ -13,8 +13,10 @@ hdfs dfs -mkdir /user/w205/final_project/flight_data
 
 echo "This script is about to run another script."
 #!/bin/bash
-load_2016.sh
-echo "This script has just run another script."
+./load_2016.sh
+
+
+echo "load_2016 finished"
 
 # Change directory back to original
 cd $MY_PWD
