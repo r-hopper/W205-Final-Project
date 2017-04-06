204a49aca4d2b35b8ac5770cf778fc5384b71b7a
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-06 15:24:42 -0400

    Update pre_post_911.sql

--- transform_flight_data/pre_post_911.sql
+++ transform_flight_data/pre_post_911.sql
@@ -1,5 +1,5 @@
 --Separate years into pre- and post-9/11 data.
---Includes 2001, because most of the year is pre-9/11.
+--Includes 2001, because most of the year is pre-9/11
 
 DROP TABLE IF EXISTS flights_pre_911;
 CREATE TABLE flights_pre_911 AS
@@ -12,3 +12,35 @@ UNION
 SELECT * FROM flights_2000
 UNION
 SELECT * FROM flights_2001;
+
+DROP TABLE IF EXISTS flights_ppst_911;
+CREATE TABLE flights_ppst_911 AS
+SELECT * FROM flights_2002
+UNION
+SELECT * FROM flights_2003
+UNION
+SELECT * FROM flights_2004
+UNION
+SELECT * FROM flights_2005
+UNION
+SELECT * FROM flights_2006
+UNION
+SELECT * FROM flights_2007
+UNION
+SELECT * FROM flights_2008
+UNION
+SELECT * FROM flights_2009
+UNION
+SELECT * FROM flights_2010
+UNION
+SELECT * FROM flights_2011
+UNION
+SELECT * FROM flights_2012
+UNION
+SELECT * FROM flights_2013
+UNION
+SELECT * FROM flights_2014
+UNION
+SELECT * FROM flights_2015
+UNION
+SELECT * FROM flights_2016;
