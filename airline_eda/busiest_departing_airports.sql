a8684fa9e17b9886e887d52cf37f3b14bb0fd9d2
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-11 14:03:46 -0400

    Update busiest_departing_airports.sql

--- airline_eda/busiest_departing_airports.sql
+++ airline_eda/busiest_departing_airports.sql
@@ -1,7 +1,7 @@
---25 busiest departing airports 2007-2016
+--25 busiest departing airports 2012-2016
 
-DROP TABLE IF EXISTS busy_2007_2016;
-CREATE TABLE busy_2007_2016 AS 
+DROP TABLE IF EXISTS busy_2012_2016;
+CREATE TABLE busy_2012_2016 AS 
 SELECT id,
        year,
        month, 
@@ -18,7 +18,7 @@ SELECT id,
        ELSE "N" END AS arrdelaygreaterthan30min,
        CASE WHEN AVG(arrdelayminutes) > 60 THEN "Y"
        ELSE "N" END AS arrdelaygreaterthan1hr
-FROM flights_2007_2016
+FROM flights_2012_2016
 WHERE id IN ('10397', '13930', '11298', '11292', '12892', '14107', '12266', '12889', '11433', '13487', '11618', '14869', 
              '14771', '13204', '10721', '11057', '12478', '12953', '10821', '14747', '14100', '14679', '13232', '11278', '15304')
 ORDER BY avgdepdelay DESC
