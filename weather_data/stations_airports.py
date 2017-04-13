e172712aab6c7eea7abbec966d441ce1c25b18e2
mike-amodeo <w205@ip-172-31-3-253.ec2.internal>
2017-04-13 12:56:25 +0000

    table linking weather stations to airports

--- weather_data/stations_airports.py
+++ weather_data/stations_airports.py
@@ -18,11 +18,16 @@ stations = pd.read_fwf("weather_stations.txt", widths = [11,9,10,7,3,31,4,4,6], 
 stations.columns = ['station_id','lat','lon','elev','state','name','GSN_FLAG','HCN_CRN_FLAG','WMO_ID']
 # Give it a country code
 stations['country'] = stations.station_id.str[0:2]
+stations_us = stations[stations.country == 'US']
+
+
 # Sort by lat lon
-stations.sort_values(['lat', 'lon'])
+#stations.sort_values(['lat', 'lon'])
 
 # Import Airport data
 airports = pd.read_csv('655213050_T_MASTER_CORD.csv')
+print(airports.columns.values)
+print(airports.head())
 # Filter down to active airports
 airports_active = airports[airports.AIRPORT_IS_LATEST == 1]
 # Drop columns we don't need
@@ -32,18 +37,21 @@ airports_active.drop(['DISPLAY_AIRPORT_NAME', 'DISPLAY_AIRPORT_CITY_NAME_FULL',
                         'CITY_MARKET_WAC', 'LAT_DEGREES', 'LAT_HEMISPHERE', 'LAT_MINUTES',
                         'LAT_SECONDS', 'LON_DEGREES', 'LON_HEMISPHERE', 'LON_MINUTES',
                         'LON_SECONDS', 'AIRPORT_START_DATE', 'AIRPORT_THRU_DATE',
-                        'AIRPORT_IS_CLOSED', 'AIRPORT_IS_LATEST', 'Unnamed: 28'])
+                        'AIRPORT_IS_CLOSED', 'AIRPORT_IS_LATEST', 'Unnamed: 28'], axis = 1)
 # Fields to keep
 #'AIRPORT_SEQ_ID', 'AIRPORT_ID', 'AIRPORT', 'AIRPORT_COUNTRY_CODE_ISO',
 #'AIRPORT_STATE_CODE', 'LATITUDE', 'LONGITUDE',
-airports_active.sort_values(['LATITUDE','LONGITUDE'])
+#airports_active.sort_values(['LATITUDE','LONGITUDE'])
+airports_active_us = airports_active[airports_active.AIRPORT_COUNTRY_CODE_ISO == 'US']
+
+output_table = pd.DataFrame(columns = ["AIRPORT_ID","AIRPORT","station_id", "distance"])
 
 # Match weather stations to airports by lat/lon
-for index, airport in airports_active.iterrows():
+i = 0
+for index, airport in airports_active_us.iterrows():
     dist_temp = 999
     stat_id = 'None'
-
-    for index, station in stations.iterrows():
+    for index2, station in stations_us.iterrows():
         if abs(airport.LATITUDE - station.lat) > 0.1:
             pass
         elif abs(airport.LONGITUDE - station.lon) > 0.1:
@@ -62,10 +70,30 @@ for index, airport in airports_active.iterrows():
                 stat_id = station.station_id
             else:
                 pass
+    print dist_temp, stat_id
+    output_table.loc[i, "AIRPORT_ID"] = airport.AIRPORT_ID
+    output_table.loc[i, "AIRPORT"] = airport.AIRPORT
+    output_table.loc[i, "distance"] = dist_temp
+    output_table.loc[i, "station_id"] = stat_id
+    print output_table.tail(1)
+    i += 1
+    print i, "out of", airports_active_us['AIRPORT'].count()
 
-    airport['distance'] = dist_temp
-    airport['station_id'] = stat_id
+#airports_active_us["distance", "station_id"] = airports_active_us.apply(func, axis = 1)
 
-# Export csv
-airports.to_csv('airports_stations.csv', columns = ['AIRPORT_SEQ_ID', 'AIRPORT_ID', 'AIRPORT', 'AIRPORT_COUNTRY_CODE_ISO',
-'AIRPORT_STATE_CODE', 'LATITUDE', 'LONGITUDE','distance','station_id'])
+    #airports_active_us.set_value(index, 'distance', dist_temp)
+    #airports_active_us.loc[index, 'distance'] = dist_temp
+    #airport['distance'] = dist_temp
+
+
+    #airport['station_id'] = stat_id
+    #airports_active_us.set_value(index, 'station_id', stat_id)
+    #airports_active_us.loc[index, 'station_id'] = stat_id
+
+
+#print airport['AIRPORT'], airport['distance'], airport['station_id']
+#print airports_active_us['station_id'].head(10)
+
+output_table.to_csv('airports_stations.csv')
+
+
