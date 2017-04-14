06b18e34d43cb2abba3ca5a8bbe7b7785fb77b35
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-14 12:35:09 -0400

    Update delay_by_routes.sql

--- airline_eda/delay_by_routes.sql
+++ airline_eda/delay_by_routes.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS routes_working_2007_2011;
 DROP TABLE IF EXISTS routes_working_2012_2016;
+DROP TABLE IF EXISTS routes_working_2007_2016;
 
 CREATE TABLE routes_working_2007_2011 AS
 SELECT year, 
@@ -113,5 +114,69 @@ FROM routes_working_2012_2016
 ORDER BY avgarrdelay DESC
 ;
 
+
+CREATE TABLE routes_working_2007_2016 AS
+SELECT year, 
+       month,
+       origin,
+       dest, 
+       count(*) AS numofflights,
+       concat(origin, "_", dest) AS route,
+       depdelayminutes,
+       arrdelayminutes,
+       MIN(depdelayminutes) AS mindepdelay,
+       MAX(depdelayminutes) AS maxdepdelay,
+       AVG(depdelayminutes) AS avgdepdelay,
+       STDDEV_POP(depdelayminutes) AS stdevdepdelay,
+       MIN(arrdelayminutes) AS minarrdelay,
+       MAX(arrdelayminutes) AS maxarrdelay,
+       AVG(arrdelayminutes) AS avgarrdelay,
+       STDDEV_POP(arrdelayminutes) AS stdevarrdelay,
+       CASE WHEN depdelayminutes > 0 THEN "Y"
+       ELSE "N" END AS depdelayed,
+       CASE WHEN depdelayminutes > 0 THEN "N"
+       ELSE "Y" END AS nodepdelay
+FROM flights_2007_2016
+GROUP BY year, 
+         month,
+         origin,
+         dest, 
+         concat(origin, "_", dest),
+         depdelayminutes,
+         arrdelayminutes
+;
+
+DROP TABLE IF EXISTS routes_2007_2016;
+CREATE TABLE routes_2007_2016 AS 
+SELECT year,
+       month, 
+       origin,
+       dest, 
+       route,       
+       depdelayminutes,
+       arrdelayminutes,
+       mindepdelay,
+       maxdepdelay,
+       avgdepdelay,
+       stdevdepdelay,
+       depdelayed,
+       nodepdelay,
+       CASE WHEN avgdepdelay > 30 THEN "Y"
+       ELSE "N" END AS depdelaygreaterthan30min,
+       CASE WHEN avgdepdelay > 60 THEN "Y"
+       ELSE "N" END AS depdelaygreaterthan1hr,
+       minarrdelay,
+       maxarrdelay,
+       avgarrdelay,
+       stdevarrdelay,
+       CASE WHEN avgarrdelay > 30 THEN "Y"
+       ELSE "N" END AS arrdelaygreaterthan30min,
+       CASE WHEN avgarrdelay > 60 THEN "Y"
+       ELSE "N" END AS arrdelaygreaterthan1hr
+FROM routes_working_2007_2016
+;
+
+
 DROP TABLE routes_working_2007_2011; 
 DROP TABLE routes_working_2012_2016; 
+DROP TABLE routes_working_2007_2016; 
