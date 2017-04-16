b929f7a36d7cbd6b552a5aac764a0a649f419c31
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-16 19:51:24 -0400

    Update more_than_2_per_day.sql

--- airline_eda/more_than_2_per_day.sql
+++ airline_eda/more_than_2_per_day.sql
@@ -4,6 +4,72 @@ DROP TABLE IF EXISTS routes_working_2007_2016_volume;
 DROP TABLE IF EXISTS routes_2007_2016_volume;
 
 CREATE TABLE routes_working_2007_2016_volume AS
-SELECT SUM(numofflights)/10 as flightsperyear,
-* FROM routes_2007_2016
-GROUP BY *;
+SELECT 
+       year,
+       month, 
+       origin,
+       dest, 
+       route,
+       numofflights,
+       depdelayminutes,
+       arrdelayminutes,
+       depdelayed,
+       nodepdelay,
+       depdelaygreaterthan30min,
+       depdelaygreaterthan1hr,
+       arrdelaygreaterthan30min,
+       arrdelaygreaterthan1hr,
+       SUM(numofflights)/10 as flightsperyear
+FROM routes_2007_2016
+GROUP BY year,
+       month, 
+       origin,
+       dest, 
+       route,
+       numofflights,
+       depdelayminutes,
+       arrdelayminutes,
+       depdelayed,
+       nodepdelay,
+       depdelaygreaterthan30min,
+       depdelaygreaterthan1hr,
+       arrdelaygreaterthan30min,
+       arrdelaygreaterthan1hr;
+       
+--Keep only airports with >2 flights per day
+CREATE TABLE routes_2007_2016_volume AS
+SELECT 
+       year,
+       month, 
+       origin,
+       dest, 
+       route,
+       numofflights,
+       depdelayminutes,
+       arrdelayminutes,
+       depdelayed,
+       nodepdelay,
+       depdelaygreaterthan30min,
+       depdelaygreaterthan1hr,
+       arrdelaygreaterthan30min,
+       arrdelaygreaterthan1hr,
+       flightsperyear
+FROM routes_working_2007_2016_volume
+GROUP BY year,
+       month, 
+       origin,
+       dest, 
+       route,
+       numofflights,
+       depdelayminutes,
+       arrdelayminutes,
+       depdelayed,
+       nodepdelay,
+       depdelaygreaterthan30min,
+       depdelaygreaterthan1hr,
+       arrdelaygreaterthan30min,
+       arrdelaygreaterthan1hr,
+       flightsperyear
+HAVING (SUM(flightsperyear) > 985);
+
+DROP TABLE routes_working_2007_2016_volume;
