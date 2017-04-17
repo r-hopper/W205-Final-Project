24ced08efbb5a41c46d1869948bef5f445b2dde0
w205 <w205@ip-172-31-12-136.ec2.internal>
2017-04-17 17:15:59 +0000

    executable files for flight + weather data

---
+++ run_all.sh
@@ -0,0 +1,53 @@
+#1/bin/bash
+
+# Load Flight Data
+# Save current directory
+MY_CWD=$(pwd)
+
+cd load_flight_data
+
+./load_flight_data.sh
+
+cd ..
+
+#Transform flight data
+cd transform_flight_data
+
+# Load data into Hive
+hive -f load_2007_2016.sql
+
+chmod 400 u+x,g+x flight_data_HDFS_cleanup.sh
+./flight_data_HDFS_cleanup.sh
+
+echo "Flight Data Loaded"
+
+# Combine monthly data to annual
+hive -f months_to_years.sql
+
+# Run all years
+hive -f all_years.sql
+
+hive -f one_to_five_years.sql
+
+hive -f cleanup_months.sql
+
+echo "Flight Data Transformed"
+
+cd ..
+
+# Load Weather Data
+
+cd weather_data
+
+chmod 400 u+x,g+x load_weather_data.sh
+./load_weather_data.sh
+
+echo "Weather Data Loaded"
+
+hive -f hive_weather_data_import.sql
+
+hive -f hive_weather_merge.sql
+
+echo "Weather Data Transformed"
+
+
---
+++ transform_flight_data/flight_data_HDFS_cleanup.sh
@@ -0,0 +1,267 @@
+#!/bin/bash
+
+# 2007
+hdfs dfs -rm final_project/flight_data/2007_1/on_time_2007_1.csv
+hdfs dfs -rmdir final_project/flight_data/2007_1
+hdfs dfs -rm final_project/flight_data/2007_2/on_time_2007_2.csv
+hdfs dfs -rmdir final_project/flight_data/2007_2
+hdfs dfs -rm final_project/flight_data/2007_3/on_time_2007_3.csv
+hdfs dfs -rmdir final_project/flight_data/2007_3
+hdfs dfs -rm final_project/flight_data/2007_4/on_time_2007_4.csv
+hdfs dfs -rmdir final_project/flight_data/2007_4
+hdfs dfs -rm final_project/flight_data/2007_5/on_time_2007_5.csv
+hdfs dfs -rmdir final_project/flight_data/2007_5
+hdfs dfs -rm final_project/flight_data/2007_6/on_time_2007_6.csv
+hdfs dfs -rmdir final_project/flight_data/2007_6
+hdfs dfs -rm final_project/flight_data/2007_7/on_time_2007_7.csv
+hdfs dfs -rmdir final_project/flight_data/2007_7
+hdfs dfs -rm final_project/flight_data/2007_8/on_time_2007_8.csv
+hdfs dfs -rmdir final_project/flight_data/2007_8
+hdfs dfs -rm final_project/flight_data/2007_9/on_time_2007_9.csv
+hdfs dfs -rmdir final_project/flight_data/2007_9
+hdfs dfs -rm final_project/flight_data/2007_10/on_time_2007_10.csv
+hdfs dfs -rmdir final_project/flight_data/2007_10
+hdfs dfs -rm final_project/flight_data/2007_11/on_time_2007_11.csv
+hdfs dfs -rmdir final_project/flight_data/2007_11
+hdfs dfs -rm final_project/flight_data/2007_12/on_time_2007_12.csv
+hdfs dfs -rmdir final_project/flight_data/2007_12
+
+# 2008
+hdfs dfs -rm final_project/flight_data/2008_1/on_time_2008_1.csv
+hdfs dfs -rmdir final_project/flight_data/2008_1
+hdfs dfs -rm final_project/flight_data/2008_2/on_time_2008_2.csv
+hdfs dfs -rmdir final_project/flight_data/2008_2
+hdfs dfs -rm final_project/flight_data/2008_3/on_time_2008_3.csv
+hdfs dfs -rmdir final_project/flight_data/2008_3
+hdfs dfs -rm final_project/flight_data/2008_4/on_time_2008_4.csv
+hdfs dfs -rmdir final_project/flight_data/2008_4
+hdfs dfs -rm final_project/flight_data/2008_5/on_time_2008_5.csv
+hdfs dfs -rmdir final_project/flight_data/2008_5
+hdfs dfs -rm final_project/flight_data/2008_6/on_time_2008_6.csv
+hdfs dfs -rmdir final_project/flight_data/2008_6
+hdfs dfs -rm final_project/flight_data/2008_7/on_time_2008_7.csv
+hdfs dfs -rmdir final_project/flight_data/2008_7
+hdfs dfs -rm final_project/flight_data/2008_8/on_time_2008_8.csv
+hdfs dfs -rmdir final_project/flight_data/2008_8
+hdfs dfs -rm final_project/flight_data/2008_9/on_time_2008_9.csv
+hdfs dfs -rmdir final_project/flight_data/2008_9
+hdfs dfs -rm final_project/flight_data/2008_10/on_time_2008_10.csv
+hdfs dfs -rmdir final_project/flight_data/2008_10
+hdfs dfs -rm final_project/flight_data/2008_11/on_time_2008_11.csv
+hdfs dfs -rmdir final_project/flight_data/2008_11
+hdfs dfs -rm final_project/flight_data/2008_12/on_time_2008_12.csv
+hdfs dfs -rmdir final_project/flight_data/2008_12
+
+# 2009
+hdfs dfs -rm final_project/flight_data/2009_1/on_time_2009_1.csv
+hdfs dfs -rmdir final_project/flight_data/2009_1
+hdfs dfs -rm final_project/flight_data/2009_2/on_time_2009_2.csv
+hdfs dfs -rmdir final_project/flight_data/2009_2
+hdfs dfs -rm final_project/flight_data/2009_3/on_time_2009_3.csv
+hdfs dfs -rmdir final_project/flight_data/2009_3
+hdfs dfs -rm final_project/flight_data/2009_4/on_time_2009_4.csv
+hdfs dfs -rmdir final_project/flight_data/2009_4
+hdfs dfs -rm final_project/flight_data/2009_5/on_time_2009_5.csv
+hdfs dfs -rmdir final_project/flight_data/2009_5
+hdfs dfs -rm final_project/flight_data/2009_6/on_time_2009_6.csv
+hdfs dfs -rmdir final_project/flight_data/2009_6
+hdfs dfs -rm final_project/flight_data/2009_7/on_time_2009_7.csv
+hdfs dfs -rmdir final_project/flight_data/2009_7
+hdfs dfs -rm final_project/flight_data/2009_8/on_time_2009_8.csv
+hdfs dfs -rmdir final_project/flight_data/2009_8
+hdfs dfs -rm final_project/flight_data/2009_9/on_time_2009_9.csv
+hdfs dfs -rmdir final_project/flight_data/2009_9
+hdfs dfs -rm final_project/flight_data/2009_10/on_time_2009_10.csv
+hdfs dfs -rmdir final_project/flight_data/2009_10
+hdfs dfs -rm final_project/flight_data/2009_11/on_time_2009_11.csv
+hdfs dfs -rmdir final_project/flight_data/2009_11
+hdfs dfs -rm final_project/flight_data/2009_12/on_time_2009_12.csv
+hdfs dfs -rmdir final_project/flight_data/2009_12
+
+# 2010
+hdfs dfs -rm final_project/flight_data/2010_1/on_time_2010_1.csv
+hdfs dfs -rmdir final_project/flight_data/2010_1
+hdfs dfs -rm final_project/flight_data/2010_2/on_time_2010_2.csv
+hdfs dfs -rmdir final_project/flight_data/2010_2
+hdfs dfs -rm final_project/flight_data/2010_3/on_time_2010_3.csv
+hdfs dfs -rmdir final_project/flight_data/2010_3
+hdfs dfs -rm final_project/flight_data/2010_4/on_time_2010_4.csv
+hdfs dfs -rmdir final_project/flight_data/2010_4
+hdfs dfs -rm final_project/flight_data/2010_5/on_time_2010_5.csv
+hdfs dfs -rmdir final_project/flight_data/2010_5
+hdfs dfs -rm final_project/flight_data/2010_6/on_time_2010_6.csv
+hdfs dfs -rmdir final_project/flight_data/2010_6
+hdfs dfs -rm final_project/flight_data/2010_7/on_time_2010_7.csv
+hdfs dfs -rmdir final_project/flight_data/2010_7
+hdfs dfs -rm final_project/flight_data/2010_8/on_time_2010_8.csv
+hdfs dfs -rmdir final_project/flight_data/2010_8
+hdfs dfs -rm final_project/flight_data/2010_9/on_time_2010_9.csv
+hdfs dfs -rmdir final_project/flight_data/2010_9
+hdfs dfs -rm final_project/flight_data/2010_10/on_time_2010_10.csv
+hdfs dfs -rmdir final_project/flight_data/2010_10
+hdfs dfs -rm final_project/flight_data/2010_11/on_time_2010_11.csv
+hdfs dfs -rmdir final_project/flight_data/2010_11
+hdfs dfs -rm final_project/flight_data/2010_12/on_time_2010_12.csv
+hdfs dfs -rmdir final_project/flight_data/2010_12
+
+# 2011
+hdfs dfs -rm final_project/flight_data/2011_1/on_time_2011_1.csv
+hdfs dfs -rmdir final_project/flight_data/2011_1
+hdfs dfs -rm final_project/flight_data/2011_2/on_time_2011_2.csv
+hdfs dfs -rmdir final_project/flight_data/2011_2
+hdfs dfs -rm final_project/flight_data/2011_3/on_time_2011_3.csv
+hdfs dfs -rmdir final_project/flight_data/2011_3
+hdfs dfs -rm final_project/flight_data/2011_4/on_time_2011_4.csv
+hdfs dfs -rmdir final_project/flight_data/2011_4
+hdfs dfs -rm final_project/flight_data/2011_5/on_time_2011_5.csv
+hdfs dfs -rmdir final_project/flight_data/2011_5
+hdfs dfs -rm final_project/flight_data/2011_6/on_time_2011_6.csv
+hdfs dfs -rmdir final_project/flight_data/2011_6
+hdfs dfs -rm final_project/flight_data/2011_7/on_time_2011_7.csv
+hdfs dfs -rmdir final_project/flight_data/2011_7
+hdfs dfs -rm final_project/flight_data/2011_8/on_time_2011_8.csv
+hdfs dfs -rmdir final_project/flight_data/2011_8
+hdfs dfs -rm final_project/flight_data/2011_9/on_time_2011_9.csv
+hdfs dfs -rmdir final_project/flight_data/2011_9
+hdfs dfs -rm final_project/flight_data/2011_10/on_time_2011_10.csv
+hdfs dfs -rmdir final_project/flight_data/2011_10
+hdfs dfs -rm final_project/flight_data/2011_11/on_time_2011_11.csv
+hdfs dfs -rmdir final_project/flight_data/2011_11
+hdfs dfs -rm final_project/flight_data/2011_12/on_time_2011_12.csv
+hdfs dfs -rmdir final_project/flight_data/2011_12
+
+# 2012
+hdfs dfs -rm final_project/flight_data/2012_1/on_time_2012_1.csv
+hdfs dfs -rmdir final_project/flight_data/2012_1
+hdfs dfs -rm final_project/flight_data/2012_2/on_time_2012_2.csv
+hdfs dfs -rmdir final_project/flight_data/2012_2
+hdfs dfs -rm final_project/flight_data/2012_3/on_time_2012_3.csv
+hdfs dfs -rmdir final_project/flight_data/2012_3
+hdfs dfs -rm final_project/flight_data/2012_4/on_time_2012_4.csv
+hdfs dfs -rmdir final_project/flight_data/2012_4
+hdfs dfs -rm final_project/flight_data/2012_5/on_time_2012_5.csv
+hdfs dfs -rmdir final_project/flight_data/2012_5
+hdfs dfs -rm final_project/flight_data/2012_6/on_time_2012_6.csv
+hdfs dfs -rmdir final_project/flight_data/2012_6
+hdfs dfs -rm final_project/flight_data/2012_7/on_time_2012_7.csv
+hdfs dfs -rmdir final_project/flight_data/2012_7
+hdfs dfs -rm final_project/flight_data/2012_8/on_time_2012_8.csv
+hdfs dfs -rmdir final_project/flight_data/2012_8
+hdfs dfs -rm final_project/flight_data/2012_9/on_time_2012_9.csv
+hdfs dfs -rmdir final_project/flight_data/2012_9
+hdfs dfs -rm final_project/flight_data/2012_10/on_time_2012_10.csv
+hdfs dfs -rmdir final_project/flight_data/2012_10
+hdfs dfs -rm final_project/flight_data/2012_11/on_time_2012_11.csv
+hdfs dfs -rmdir final_project/flight_data/2012_11
+hdfs dfs -rm final_project/flight_data/2012_12/on_time_2012_12.csv
+hdfs dfs -rmdir final_project/flight_data/2012_12
+
+# 2013
+hdfs dfs -rm final_project/flight_data/2013_1/on_time_2013_1.csv
+hdfs dfs -rmdir final_project/flight_data/2013_1
+hdfs dfs -rm final_project/flight_data/2013_2/on_time_2013_2.csv
+hdfs dfs -rmdir final_project/flight_data/2013_2
+hdfs dfs -rm final_project/flight_data/2013_3/on_time_2013_3.csv
+hdfs dfs -rmdir final_project/flight_data/2013_3
+hdfs dfs -rm final_project/flight_data/2013_4/on_time_2013_4.csv
+hdfs dfs -rmdir final_project/flight_data/2013_4
+hdfs dfs -rm final_project/flight_data/2013_5/on_time_2013_5.csv
+hdfs dfs -rmdir final_project/flight_data/2013_5
+hdfs dfs -rm final_project/flight_data/2013_6/on_time_2013_6.csv
+hdfs dfs -rmdir final_project/flight_data/2013_6
+hdfs dfs -rm final_project/flight_data/2013_7/on_time_2013_7.csv
+hdfs dfs -rmdir final_project/flight_data/2013_7
+hdfs dfs -rm final_project/flight_data/2013_8/on_time_2013_8.csv
+hdfs dfs -rmdir final_project/flight_data/2013_8
+hdfs dfs -rm final_project/flight_data/2013_9/on_time_2013_9.csv
+hdfs dfs -rmdir final_project/flight_data/2013_9
+hdfs dfs -rm final_project/flight_data/2013_10/on_time_2013_10.csv
+hdfs dfs -rmdir final_project/flight_data/2013_10
+hdfs dfs -rm final_project/flight_data/2013_11/on_time_2013_11.csv
+hdfs dfs -rmdir final_project/flight_data/2013_11
+hdfs dfs -rm final_project/flight_data/2013_12/on_time_2013_12.csv
+hdfs dfs -rmdir final_project/flight_data/2013_12
+
+# 2014
+hdfs dfs -rm final_project/flight_data/2014_1/on_time_2014_1.csv
+hdfs dfs -rmdir final_project/flight_data/2014_1
+hdfs dfs -rm final_project/flight_data/2014_2/on_time_2014_2.csv
+hdfs dfs -rmdir final_project/flight_data/2014_2
+hdfs dfs -rm final_project/flight_data/2014_3/on_time_2014_3.csv
+hdfs dfs -rmdir final_project/flight_data/2014_3
+hdfs dfs -rm final_project/flight_data/2014_4/on_time_2014_4.csv
+hdfs dfs -rmdir final_project/flight_data/2014_4
+hdfs dfs -rm final_project/flight_data/2014_5/on_time_2014_5.csv
+hdfs dfs -rmdir final_project/flight_data/2014_5
+hdfs dfs -rm final_project/flight_data/2014_6/on_time_2014_6.csv
+hdfs dfs -rmdir final_project/flight_data/2014_6
+hdfs dfs -rm final_project/flight_data/2014_7/on_time_2014_7.csv
+hdfs dfs -rmdir final_project/flight_data/2014_7
+hdfs dfs -rm final_project/flight_data/2014_8/on_time_2014_8.csv
+hdfs dfs -rmdir final_project/flight_data/2014_8
+hdfs dfs -rm final_project/flight_data/2014_9/on_time_2014_9.csv
+hdfs dfs -rmdir final_project/flight_data/2014_9
+hdfs dfs -rm final_project/flight_data/2014_10/on_time_2014_10.csv
+hdfs dfs -rmdir final_project/flight_data/2014_10
+hdfs dfs -rm final_project/flight_data/2014_11/on_time_2014_11.csv
+hdfs dfs -rmdir final_project/flight_data/2014_11
+hdfs dfs -rm final_project/flight_data/2014_12/on_time_2014_12.csv
+hdfs dfs -rmdir final_project/flight_data/2014_12
+
+# 2015
+hdfs dfs -rm final_project/flight_data/2015_1/on_time_2015_1.csv
+hdfs dfs -rmdir final_project/flight_data/2015_1
+hdfs dfs -rm final_project/flight_data/2015_2/on_time_2015_2.csv
+hdfs dfs -rmdir final_project/flight_data/2015_2
+hdfs dfs -rm final_project/flight_data/2015_3/on_time_2015_3.csv
+hdfs dfs -rmdir final_project/flight_data/2015_3
+hdfs dfs -rm final_project/flight_data/2015_4/on_time_2015_4.csv
+hdfs dfs -rmdir final_project/flight_data/2015_4
+hdfs dfs -rm final_project/flight_data/2015_5/on_time_2015_5.csv
+hdfs dfs -rmdir final_project/flight_data/2015_5
+hdfs dfs -rm final_project/flight_data/2015_6/on_time_2015_6.csv
+hdfs dfs -rmdir final_project/flight_data/2015_6
+hdfs dfs -rm final_project/flight_data/2015_7/on_time_2015_7.csv
+hdfs dfs -rmdir final_project/flight_data/2015_7
+hdfs dfs -rm final_project/flight_data/2015_8/on_time_2015_8.csv
+hdfs dfs -rmdir final_project/flight_data/2015_8
+hdfs dfs -rm final_project/flight_data/2015_9/on_time_2015_9.csv
+hdfs dfs -rmdir final_project/flight_data/2015_9
+hdfs dfs -rm final_project/flight_data/2015_10/on_time_2015_10.csv
+hdfs dfs -rmdir final_project/flight_data/2015_10
+hdfs dfs -rm final_project/flight_data/2015_11/on_time_2015_11.csv
+hdfs dfs -rmdir final_project/flight_data/2015_11
+hdfs dfs -rm final_project/flight_data/2015_12/on_time_2015_12.csv
+hdfs dfs -rmdir final_project/flight_data/2015_12
+
+# 2016
+hdfs dfs -rm final_project/flight_data/2016_1/on_time_2016_1.csv
+hdfs dfs -rmdir final_project/flight_data/2016_1
+hdfs dfs -rm final_project/flight_data/2016_2/on_time_2016_2.csv
+hdfs dfs -rmdir final_project/flight_data/2016_2
+hdfs dfs -rm final_project/flight_data/2016_3/on_time_2016_3.csv
+hdfs dfs -rmdir final_project/flight_data/2016_3
+hdfs dfs -rm final_project/flight_data/2016_4/on_time_2016_4.csv
+hdfs dfs -rmdir final_project/flight_data/2016_4
+hdfs dfs -rm final_project/flight_data/2016_5/on_time_2016_5.csv
+hdfs dfs -rmdir final_project/flight_data/2016_5
+hdfs dfs -rm final_project/flight_data/2016_6/on_time_2016_6.csv
+hdfs dfs -rmdir final_project/flight_data/2016_6
+hdfs dfs -rm final_project/flight_data/2016_7/on_time_2016_7.csv
+hdfs dfs -rmdir final_project/flight_data/2016_7
+hdfs dfs -rm final_project/flight_data/2016_8/on_time_2016_8.csv
+hdfs dfs -rmdir final_project/flight_data/2016_8
+hdfs dfs -rm final_project/flight_data/2016_9/on_time_2016_9.csv
+hdfs dfs -rmdir final_project/flight_data/2016_9
+hdfs dfs -rm final_project/flight_data/2016_10/on_time_2016_10.csv
+hdfs dfs -rmdir final_project/flight_data/2016_10
+hdfs dfs -rm final_project/flight_data/2016_11/on_time_2016_11.csv
+hdfs dfs -rmdir final_project/flight_data/2016_11
+hdfs dfs -rm final_project/flight_data/2016_12/on_time_2016_12.csv
+hdfs dfs -rmdir final_project/flight_data/2016_12
+
+hdfs dfs -rmdir final_project/flight_data
+
+
+
+
