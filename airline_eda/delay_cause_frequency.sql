dd2f6a52477c556dd9b0b0a7c26d6450d33a7bee
Michael Alexander <Malexander@comscore.com>
2017-04-18 20:16:03 -0400

    delay queries and data export

---
+++ airline_eda/delay_cause_frequency.sql
@@ -0,0 +1,60 @@
+
+DROP TABLE IF EXISTS origin_delay_cause;
+CREATE TABLE origin_delay_cause AS
+SELECT
+      origin,
+      SUM(carrierdelay) AS carrierdelay_total,
+      SUM(weatherdelay) AS weatherdelay_total,
+      SUM(nasdelay) AS nasdelay_total,
+      SUM(securitydelay) AS securitydelay_total,
+      SUM(lateaircraftdelay) AS lateaircraftdelay_total,
+      SUM(depdelay) AS depdelay_total,
+      SUM(arrdelay) AS arrdelay_total,
+      SUM(depdelayminutes) AS depdelayminutes_total,
+      SUM(arrdelayminutes) AS arrdelayminutes_total,
+      COUNT(*) as flights_total
+FROM flights_2012_2016
+GROUP BY origin
+;
+
+DROP TABLE IF EXISTS origin_delay_cause_max;
+CREATE TABLE origin_delay_cause_max AS
+SELECT
+      origin,
+      CASE WHEN carrierdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS carrierdelay_greatest,
+      CASE WHEN weatherdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS weatherdelay_greatest,
+      CASE WHEN nasdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS nasdelay_greatest,
+      CASE WHEN securitydelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS securitydelay_greatest,
+      CASE WHEN lateaircraftdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS lateaircraftdelay_greatest
+FROM origin_delay_cause
+GROUP BY origin, carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total
+;
+
+DROP TABLE IF EXISTS delay_cause_total;
+CREATE TABLE delay_cause_total AS
+SELECT
+      SUM(carrierdelay_total) AS carrierdelay_total,
+      SUM(weatherdelay_total) AS weatherdelay_total,
+      SUM(nasdelay_total) AS nasdelay_total,
+      SUM(securitydelay_total) AS securitydelay_total,
+      SUM(lateaircraftdelay_total) AS lateaircraftdelay_total
+FROM origin_delay_cause
+;
+
+DROP TABLE IF EXISTS delay_cause_total_year;
+CREATE TABLE delay_cause_total_year AS
+SELECT
+      year,
+      SUM(carrierdelay) AS carrierdelay_total,
+      SUM(weatherdelay) AS weatherdelay_total,
+      SUM(nasdelay) AS nasdelay_total,
+      SUM(securitydelay) AS securitydelay_total,
+      SUM(lateaircraftdelay) AS lateaircraftdelay_total
+FROM flights_2007_2016
+GROUP BY year
+;
---
+++ airline_eda/delay_dayofweek.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS delay_dayofweek;
+CREATE TABLE delay_dayofweek AS
+SELECT
+      origin,
+      dayofweek,
+      SUM(depdelayminutes) AS depdelayminutes_total,
+      SUM(arrdelayminutes) AS arrdelayminutes_total,
+      COUNT(*) as flights_total
+FROM flights_2012_2016
+GROUP BY origin, dayofweek
+;
+
+DROP TABLE IF EXISTS worst_dayofweek;
+CREATE TABLE worst_dayofweek AS
+SELECT
+      origin,
+      dayofweek,
+      (depdelayminutes_total+arrdelayminutes_total)/flights_total AS delay_ratio
+FROM flights_2012_2016
+;
