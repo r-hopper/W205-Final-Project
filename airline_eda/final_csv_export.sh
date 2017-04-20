3be1e7822c0aa9cfbbd98b48f341c84d028d1f87
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-20 14:28:16 -0400

    Update final_csv_export.sh

--- airline_eda/final_csv_export.sh
+++ airline_eda/final_csv_export.sh
@@ -2,23 +2,25 @@
 
 # Run airline queries in hive
 
-hive -e 'SELECT route, AVG(depdelayminutes) as avgdepdelay FROM routes_2007_2016 GROUP BY route
-ORDER BY avgdepdelay DESC LIMIT 25' > dep_delay_route.tsv
+hive -e 'set hive.cli.print.header=true; SELECT route, AVG(depdelayminutes) as avgdepdelay FROM routes_2007_2016 GROUP BY route
+ORDER BY avgdepdelay DESC LIMIT 25'| sed 's/[\t]/,/g' > dep_delay_route.tsv
 
-hive -e 'SELECT route, AVG(arrdelayminutes) as avgarrdelay FROM routes_2007_2016 GROUP BY route
-ORDER BY avgarrdelay DESC LIMIT 25' > arr_delay_route.tsv
+hive -e 'set hive.cli.print.header=true; SELECT route, AVG(arrdelayminutes) as avgarrdelay FROM routes_2007_2016 GROUP BY route
+ORDER BY avgarrdelay DESC LIMIT 25' | sed 's/[\t]/,/g' > arr_delay_route.tsv
 
-hive -e 'SELECT origin, SUM(depdelayed)/SUM(numofflights) as delayprop, SUM(numofflights)/10 as flightsperyear
+hive -e 'set hive.cli.print.header=true; SELECT origin, SUM(depdelayed)/SUM(numofflights) as delayprop, 
+SUM(numofflights)/10 as flightsperyear
 FROM top100_routes_2007_2016_del
 GROUP BY origin
 HAVING (SUM(numofflights)/10 > 985)
-ORDER BY delayprop DESC LIMIT 25' > highest_delay_prop.tsv
+ORDER BY delayprop DESC LIMIT 25' | sed 's/[\t]/,/g' > highest_delay_prop.tsv
 
-hive -e 'SELECT origin, SUM(longdelay)/SUM(numofflights) as delayprop, SUM(numofflights)/10 as flightsperyear
+hive -e 'set hive.cli.print.header=true; SELECT origin, SUM(longdelay)/SUM(numofflights) as delayprop, 
+SUM(numofflights)/10 as flightsperyear
 FROM top100_routes_2007_2016_del
 GROUP BY origin
 HAVING (SUM(numofflights)/10 > 985)
-ORDER BY delayprop DESC LIMIT 25' > highest_long_delay_prop.tsv
+ORDER BY delayprop DESC LIMIT 25' | sed 's/[\t]/,/g' > highest_long_delay_prop.tsv
 
 cd ..
 mv airline_eda/dep_delay_route.tsv flight_app/final_csv/dep_delay_route.tsv
