8dfad24a4a59aba6c67409ef318ad27ed8c5e835
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-20 11:26:31 -0400

    Update queries_for_csv_export.sql

--- airline_eda/queries_for_csv_export.sql
+++ airline_eda/queries_for_csv_export.sql
@@ -8,7 +8,7 @@ SELECT route, avgarrdelay FROM routes_2007_2016 GROUP BY route, avgarrdelay ORDE
 --Airports with Highest Percentage of Delayed Flights (threshold for min number of flights)
 --Airports with >2 flights per day -- eliminates 33% of distinct origins
 SELECT origin, SUM(depdelayed)/SUM(numofflights) as delayprop, SUM(numofflights)/10 as flightsperyear
-FROM top100_routes_2007_2016
+FROM top100_routes_2007_2016_del
 GROUP BY origin
 HAVING (SUM(numofflights)/10 > 985)
 ORDER BY delayprop DESC LIMIT 25;
