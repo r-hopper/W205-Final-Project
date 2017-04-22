85f20c15c2085283989f518b5139a82202bc9921
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-22 17:09:40 -0400

    Delete exporting_query_results.sh

--- exporting_query_results/exporting_query_results.sh
+++
@@ -1,3 +0,0 @@
-hive -e 'set hive.cli.print.header=true; select * from worst_dayofweek_final' | sed 's/[\t]/,/g'  > /home/w205/W205-Final-Project/flight_app/final_csv/worst_dayofweek_final.csv
-hive -e 'set hive.cli.print.header=true; select * from origin_most_frequent_delay_cause' | sed 's/[\t]/,/g'  > /home/w205/W205-Final-Project/flight_app/final_csv/origin_most_frequent_delay_cause.csv
-hive -e 'set hive.cli.print.header=true; select * from delay_cause_total_year' | sed 's/[\t]/,/g'  > /home/w205/W205-Final-Project/flight_app/final_csv/delay_cause_total_year.csv
