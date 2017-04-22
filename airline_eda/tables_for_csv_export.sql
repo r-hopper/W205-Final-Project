24bbbb7d8fd979cc1a49c806cb2749faba72ce99
Roseanna Hopper <rhopper@berkeley.edu>
2017-04-22 16:57:50 -0400

    Update tables_for_csv_export.sql

--- airline_eda/tables_for_csv_export.sql
+++ airline_eda/tables_for_csv_export.sql
@@ -1,5 +1,5 @@
+--Adding route and binaries for departure delay
 DROP TABLE IF EXISTS routes_working_2007_2016;
-
 CREATE TABLE routes_working_2007_2016 AS
 SELECT year, 
        month,
@@ -56,7 +56,6 @@ GROUP BY year,
        nodepdelay
 ;
 
-
 DROP TABLE IF EXISTS routes_2007_2016_del;
 CREATE TABLE routes_2007_2016_del AS 
 SELECT year,
@@ -88,6 +87,7 @@ GROUP BY year,
        arrdelaygreaterthan1hr
 ;
 
+--Selecting airports where within the top 100 origins by departure volume
 DROP TABLE IF EXISTS top100_routes_2007_2016_del;
 CREATE TABLE top100_routes_2007_2016_del AS 
 SELECT year,
@@ -121,3 +121,311 @@ GROUP BY year,
 ;
 
 DROP TABLE routes_working_2007_2016; 
+
+
+--Finding total delayed flights and minutes per weekday per origin airport from 2007 - 2016
+DROP TABLE IF EXISTS delay_dayofweek;
+CREATE TABLE delay_dayofweek AS
+SELECT
+      origin,
+      dayofweek,
+      SUM(depdelayminutes) AS depdelayminutes_total,
+      SUM(arrdelayminutes) AS arrdelayminutes_total,
+      COUNT(*) as flights_total
+FROM flights_2007_2016
+GROUP BY origin, dayofweek
+;
+
+--Calculating ratio of delayed minutes / flights
+DROP TABLE IF EXISTS worst_dayofweek;
+CREATE TABLE worst_dayofweek AS
+SELECT
+      origin,
+      dayofweek,
+      (depdelayminutes_total+arrdelayminutes_total)/flights_total AS delay_ratio
+FROM delay_dayofweek
+;
+
+--Identifying day of the week with the highest ratio for each origin airport
+DROP TABLE IF EXISTS worst_dayofweek_final;
+CREATE TABLE worst_dayofweek_final AS
+SELECT
+	a.origin,
+	CASE WHEN a.dayofweek = 1 THEN "Monday"
+		WHEN a.dayofweek = 2 THEN "Tuesday"
+		WHEN a.dayofweek = 3 THEN "Wednesday"
+		WHEN a.dayofweek = 4 THEN "Thursday"
+		WHEN a.dayofweek = 5 THEN "Friday"
+		WHEN a.dayofweek = 6 THEN "Saturday"
+		WHEN a.dayofweek = 7 THEN "Sunday"
+		ELSE "Unknown" END AS worst_dayofweek
+FROM worst_dayofweek a
+INNER JOIN (
+	SELECT
+		origin,
+		MAX(delay_ratio) AS delay_ratio
+	FROM worst_dayofweek
+	GROUP BY origin
+) b
+ON a.origin = b.origin
+AND a.delay_ratio = b.delay_ratio
+;
+
+--Finding total delayed flights and minutes per cause per origin airport from 2007 - 2016
+DROP TABLE IF EXISTS origin_delay_cause;
+CREATE TABLE origin_delay_cause AS
+SELECT
+      origin,
+      SUM(carrierdelay) AS carrierdelay_total,
+      SUM(weatherdelay) AS weatherdelay_total,
+      SUM(nasdelay) AS nasdelay_total,
+      SUM(securitydelay) AS securitydelay_total,
+      SUM(lateaircraftdelay) AS lateaircraftdelay_total,
+      SUM(depdelay) AS depdelay_total,
+      SUM(arrdelay) AS arrdelay_total,
+      SUM(depdelayminutes) AS depdelayminutes_total,
+      SUM(arrdelayminutes) AS arrdelayminutes_total,
+      COUNT(*) as flights_total
+FROM flights_2007_2016
+GROUP BY origin
+;
+
+--Identifying delay cause with the highest total minutes per origin airport
+DROP TABLE IF EXISTS origin_delay_cause_max;
+CREATE TABLE origin_delay_cause_max AS
+SELECT
+      origin,
+      CASE WHEN carrierdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS carrierdelay_greatest,
+      CASE WHEN weatherdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS weatherdelay_greatest,
+      CASE WHEN nasdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS nasdelay_greatest,
+      CASE WHEN securitydelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS securitydelay_greatest,
+      CASE WHEN lateaircraftdelay_total = GREATEST(carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total)
+            THEN 1 ELSE 0 END AS lateaircraftdelay_greatest
+FROM origin_delay_cause
+GROUP BY origin, carrierdelay_total, weatherdelay_total, nasdelay_total, securitydelay_total, lateaircraftdelay_total
+;
+
+--Finding total delay minutes per cause per year from 2007 - 2016
+DROP TABLE IF EXISTS delay_cause_total_year;
+CREATE TABLE delay_cause_total_year AS
+SELECT
+      year,
+      CAST(SUM(carrierdelay) AS DECIMAL) AS carrierdelay_total,
+      CAST(SUM(weatherdelay) AS DECIMAL)AS weatherdelay_total,
+      CAST(SUM(nasdelay) AS DECIMAL) AS nasdelay_total,
+      CAST(SUM(securitydelay) AS DECIMAL) AS securitydelay_total,
+      CAST(SUM(lateaircraftdelay) AS DECIMAL) AS lateaircraftdelay_total
+FROM flights_2007_2016
+GROUP BY year
+;
+
+--Formatting most frequent delay cause per origin
+DROP TABLE IF EXISTS origin_most_frequent_delay_cause;
+CREATE TABLE origin_most_frequent_delay_cause AS
+SELECT
+      origin,
+      CASE WHEN carrierdelay_greatest+weatherdelay_greatest+nasdelay_greatest+securitydelay_greatest+lateaircraftdelay_greatest <> 1 THEN "multiple"
+            WHEN carrierdelay_greatest = 1 THEN "carrier_delay"
+            WHEN weatherdelay_greatest = 1 THEN "weather_delay"
+            WHEN nasdelay_greatest = 1 THEN "national_air_system_delay"
+            WHEN securitydelay_greatest = 1 THEN "security_delay"
+            WHEN lateaircraftdelay_greatest = 1 THEN "late_arriving_aircraft_delay"
+            END AS most_frequent_delay_cause
+FROM origin_delay_cause_max
+;
+
+--Airport arrival volume time series
+create table busy_arr_airport_2016 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2016
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+create table busy_arr_airport_2015 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2015
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+create table busy_arr_airport_2014 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2014
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+create table busy_arr_airport_2013 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2013
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+create table busy_arr_airport_2012 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2012
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+
+create table busy_arr_airport_2011 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2011
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+
+create table busy_arr_airport_2010 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2010
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+create table busy_arr_airport_2009 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2009
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+
+create table busy_arr_airport_2008 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2008
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+create table busy_arr_airport_2007 as
+select DestAirportID, Dest, count(*) as RS 
+from flights_2007
+group by DestAirportID, Dest
+order by RS desc
+limit 30;
+
+select a07.DestAirportID, a16.Dest, a07.RS, a08.RS, a09.RS, a10.RS, a11.RS, a12.RS, a13.RS, a14.RS, a15.RS, a16.RS 
+from busy_arr_airport_2007 a07
+inner join busy_arr_airport_2008 a08
+on a07.DestAirportID = a08.DestAirportID
+inner join busy_arr_airport_2009 a09
+on a08.DestAirportID = a09.DestAirportID
+inner join busy_arr_airport_2010 a10
+on a09.DestAirportID = a10.DestAirportID
+inner join busy_arr_airport_2011 a11
+on a10.DestAirportID = a11.DestAirportID
+inner join busy_arr_airport_2012 a12
+on a11.DestAirportID = a12.DestAirportID
+inner join busy_arr_airport_2013 a13
+on a12.DestAirportID = a13.DestAirportID
+inner join busy_arr_airport_2014 a14
+on a13.DestAirportID = a14.DestAirportID
+inner join busy_arr_airport_2015 a15
+on a14.DestAirportID = a15.DestAirportID
+inner join busy_arr_airport_2016 a16
+on a15.DestAirportID = a16.DestAirportID;
+
+--Airport departure volume time series
+create table busy_dep_airport_2016 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2016
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+create table busy_dep_airport_2015 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2015
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+create table busy_dep_airport_2014 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2014
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+create table busy_dep_airport_2013 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2013
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+create table busy_dep_airport_2012 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2012
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+
+create table busy_dep_airport_2011 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2011
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+
+create table busy_dep_airport_2010 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2010
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+
+create table busy_dep_airport_2009 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2009
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+
+create table busy_dep_airport_2008 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2008
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+
+create table busy_dep_airport_2007 as
+select OriginAirportID, Origin, count(*) as RS 
+from flights_2007
+group by OriginAirportID, Origin
+order by RS desc
+limit 30;
+
+select a07.OriginAirportID, a16.Origin, a07.RS, a08.RS, a09.RS, a10.RS, a11.RS, a12.RS, a13.RS, a14.RS, a15.RS, a16.RS 
+from busy_dep_airport_2007 a07
+inner join busy_dep_airport_2008 a08
+on a07.OriginAirportID = a08.OriginAirportID
+inner join busy_dep_airport_2009 a09
+on a08.OriginAirportID = a09.OriginAirportID
+inner join busy_dep_airport_2010 a10
+on a09.OriginAirportID = a10.OriginAirportID
+inner join busy_dep_airport_2011 a11
+on a10.OriginAirportID = a11.OriginAirportID
+inner join busy_dep_airport_2012 a12
+on a11.OriginAirportID = a12.OriginAirportID
+inner join busy_dep_airport_2013 a13
+on a12.OriginAirportID = a13.OriginAirportID
+inner join busy_dep_airport_2014 a14
+on a13.OriginAirportID = a14.OriginAirportID
+inner join busy_dep_airport_2015 a15
+on a14.OriginAirportID = a15.OriginAirportID
+inner join busy_dep_airport_2016 a16
+on a15.OriginAirportID = a16.OriginAirportID;
